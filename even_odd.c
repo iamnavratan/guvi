ad40eb093d2de8a89d60584cbc6991cde287710b
iamnavratan <navratanasdev@gmail.com>
2017-08-27 19:47:46 +0530

    C program

---
+++ even_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num;
+	printf("Enter a number\n");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("Even Number\n");
+	else
+	printf("Odd NUmber\n");
+	getch();
+	return 0;
+}
